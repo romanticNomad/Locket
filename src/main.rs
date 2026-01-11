e9dd7c0bb2b9b288151afcb0ec3e57262c6bfccb
romanticNomad <satvolume@gmail.com>
2026-01-11 16:50:51 +0000

    key gen prototype

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use k256::ecdsa::SigningKey;
-use k256::elliptic_curve::sec1::ToEncodedPoint;
 use rand::rngs::OsRng;
 use sha3::{Digest, Keccak256};
+use hex::encode;
 
 fn main() {
     // 1. Generate private key
@@ -10,7 +10,7 @@ fn main() {
 
     // 2. Private key bytes
     let private_key_bytes = signing_key.to_bytes();
-    println!("Private key: 0x{}", hex::encode(private_key_bytes));
+    println!("Private key: 0x{}", encode(private_key_bytes));
 
     // 3. Uncompressed public key (0x04 || X || Y)
     let public_key = verify_key.to_encoded_point(false);
@@ -20,5 +20,5 @@ fn main() {
     let hash = Keccak256::digest(&pubkey_bytes[1..]);
     let address = &hash[12..];
 
-    println!("Address: 0x{}", hex::encode(address));
+    println!("Address: 0x{}", encode(address));
 }
